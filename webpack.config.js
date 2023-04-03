7de651e26e17f5bd13c6de8d8dd6b197b8c1a826
Aveapp-Trivien <trivien@aveapp.com>
2023-04-03 08:55:41 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "./dist/"),
     },
-    // historyApiFallback: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
